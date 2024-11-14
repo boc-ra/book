9eaf177c73b049aef2f04050054a3d9af0038e18
boc-ra <s7060smnju@gmail.com>
2024-11-14 22:04:04 +0900

    '読書記録アプリ'

---
+++ myproject/bookdiary/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Bookdiary
+
+
+class BookdiaryForm(forms.ModelForm):
+    class Meta:
+        model = Bookdiary
+        fields = ('date', 'title', 'writer','text',)
+        widgets = {
+            'date': forms.DateInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'writer': forms.TextInput(attrs={'class': 'form-control'}),
+            'text': forms.Textarea(attrs={'class': 'form-control'}),
+        }
